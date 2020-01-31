32e6caf9c9d18197a10dea2afcec50d7a76e68db
dgu-pmos <gudah1478@dongguk.edu>
2020-01-31 12:01:24 +0900

    edit CORS issue

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+  next();
+});
+
 app.use(methodOverride('_method'));
 
 let corsOptions = {
