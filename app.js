c173f480e66d3add8cf813894e5e937a562ccd16
dgu-pmos <gudah1478@dongguk.edu>
2020-01-31 12:08:04 +0900

    edit cors2

--- app.js
+++ app.js
@@ -29,17 +29,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/*
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
   next();
 });
+*/
 
 app.use(methodOverride('_method'));
 
 let corsOptions = {
-  origin: '*',
+  origin: '211.63.192.209:3000',
   credentials: true
 }
 
